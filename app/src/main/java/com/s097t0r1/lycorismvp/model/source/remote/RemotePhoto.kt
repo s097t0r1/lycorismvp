cabb1bf7c941a9f5cde3d2f97f61d0ad935a2e44
s097t0r1 <zmitr241@yandex.ru>
2020-11-28 16:22:36 +0300

    feat(model.source.local) add API service and network model

---
+++ app/src/main/java/com/s097t0r1/lycorismvp/model/source/remote/RemotePhoto.kt
@@ -0,0 +1,7 @@
+package com.s097t0r1.lycorismvp.model.source.remote
+
+data class RemotePhoto(
+    val id: String,
+    val imageUrl: String,
+    val description: String
+)
---
+++ app/src/main/java/com/s097t0r1/lycorismvp/model/source/remote/UnsplashAPI.kt
@@ -0,0 +1,22 @@
+package com.s097t0r1.lycorismvp.model.source.remote
+
+import io.reactivex.Maybe
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+const val BASE_URL = "https://api.unsplash.com/"
+const val ACCESS_KEY = "LRz23zhofhEVGQQmZzILVnBHb8ZpTGhYlQWkdRNY5OA"
+const val AUTH_HEADER = "Authorization: Client-ID $ACCESS_KEY"
+
+interface UnsplashAPI {
+
+    @Headers(AUTH_HEADER)
+    @GET("photos")
+    fun getPhotos(): Observable<String>
+
+    @Headers(AUTH_HEADER)
+    @GET("photos/{id}")
+    fun getPhoto(@Path("id")id: String): Observable<String>
+}
